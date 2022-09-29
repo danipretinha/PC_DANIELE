2bd98b1692fb13df1afb63b16113e2021c09f84c
Dani alves <112084683+danipretinha@users.noreply.github.com>
2022-09-29 08:52:29 -0300

    29/09slyle

---
+++ index.c
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html> lang="pt-br">
+<head>
+<meta charet="UTF-8">
+<title>Barbearia Alura</title>
+</head>
+<body>
+<p><h1 style= "tex-align:center">Sobre a Barbearia Alura</p></h1>
+
+<p style= "tex-align:center">Localizada no coração da cidade a Barbearia Alura traz para o mercado o que há de melhor para o seu cabelo e barba. Fundada em 2019, a <strong> Barbearia Alura </strong> já é destaque na cidade e conquista novos clientes a cada dia.</p>
+
+<p style= "tex-align:center">Nossa missão é:<em><strong>"Proporcionar auto-estima e qualidade de vida aos clientes".</strong></em>.</p>
+
+<p style= "tex-align:center">Oferecemos profissionais experientes e antenados às mudanças no mundo da moda. O atendimento possui padrão de excelência e agilidade, garantindo qualidade e satisfação dos nossos clientes.,</p>
+</body>
+</html>
+
+><h1>Sobre a Barbearia Alura</h1></p>
+
+<p style= "tex-align:center">Localizada no coração da cidade a Barbearia Alura traz para o mercado o que há de melhor para o seu cabelo e barba. Fundada em 2019, a <strong> Barbearia Alura </strong> já é destaque na cidade e conquista novos clientes a cada dia.</p>
+
+<p style= "tex-align:center">Nossa missão é:<em><strong>"Proporcionar auto-estima e qualidade de vida aos clientes".</strong></em>.</p>
+
+<p style= "tex-align:center">Oferecemos profissionais experientes e antenados às mudanças no mundo da moda. O atendimento possui padrão de excelência e agilidade, garantindo qualidade e satisfação dos nossos clientes.,</p>
+to possui padrão de excelência e agilidade, garantindo qualidade e satisfação dos nossos clientes.,</p>
+</body>
+</html>
+
+
